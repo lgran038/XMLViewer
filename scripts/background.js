6a27de224db9aa969849689b9d9dabfb7df65bb0
Luis Granados <lgran038@fiu.edu>
2018-09-25 14:44:47 -0400

    Expand and Collapse children both work. Should refactor messagin to only call functions and not have logic

--- scripts/background.js
+++ scripts/background.js
@@ -9,11 +9,32 @@ function collapseChildren(e){
     });
 }
 
+//Collapse Children context menu item
+function expandChildren(e){
+    chrome.storage.local.get(['childRgtClkIDValid', 'childClickedID'], function(result){
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+            chrome.tabs.sendMessage(tabs[0].id, {
+                message: "expandChildren",
+                result: result});
+          });
+    });
+}
+
+//URLs where context menus should be added
+var urlPatterns = ["http://*/*.xml", "https://*/*.xml", "file:///*.xml"];
 //Creates context menu for Collapse Children
 chrome.contextMenus.create({
     title: "Collapse Children", 
     contexts:["page"], 
-    onclick: collapseChildren
+    onclick: collapseChildren,
+    documentUrlPatterns: urlPatterns
+});
+
+chrome.contextMenus.create({
+    title: "Expand Children", 
+    contexts:["page"], 
+    onclick: expandChildren,
+    documentUrlPatterns: urlPatterns
 });
 
 //Listener for messages
--- scripts/content.js
+++ scripts/content.js
@@ -17,6 +17,12 @@ function collapse(node){
     node.children[1].className = "collapsed";
 }
 
+//Expand a node
+function expand(node){
+    node.children[0].className = "expanded";
+    node.children[1].className = "collapsed hidden";
+}
+
 //Returns all collapsible children
 //Input node must be collapsible
 function getCollapsibleChildren(node){
@@ -69,6 +75,7 @@ function onElementClick(e){
 }
 
 //Listends for messages from background script
+//NOTE: SHOULD REFACTOR. ON MESSAGE LISTENER SHOULD MAKE FUNCTION CALLS NOT APPLY LOGIC
 chrome.runtime.onMessage.addListener(function(e) {
     
     //Listening for collapseChildren message
@@ -80,9 +87,26 @@ chrome.runtime.onMessage.addListener(function(e) {
         if (e.result.childClickedID && e.result.childRgtClkIDValid){
             var node = document.getElementById(e.result.childClickedID);
             var children = this.getCollapsibleChildren(node);
-
+            //Collapse children
             for (child of children)
                 this.collapse(child);
+        }
+    }
+
+    //Listening for expandChildren message
+    if(e.message === "expandChildren"){
+        //Reset childRgtClkIDValid
+        chrome.storage.local.set({childRgtClkIDValid: false});
+
+        //Collapse children if child exists and is valid
+        if (e.result.childClickedID && e.result.childRgtClkIDValid){
+            var node = document.getElementById(e.result.childClickedID);
+            //Expand itself if collapsed
+            this.expand(node);
+            var children = this.getCollapsibleChildren(node);
+            //Expand children
+            for (child of children)
+                this.expand(child);
         }
     }
 
